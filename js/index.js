a6ef495d87c4bdb9c2bc63ef69775293a781803c
Paulo <paulorzzbj@gmail.com>
2026-02-16 10:31:20 +0100

    hero string

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 // slå dig løs her... 
 
+// Hero
 const heroSec = document.querySelector(".hero");
-let heroImage = document.createElement("img");
+
+/* let heroImage = document.createElement("img");
 heroImage.setAttribute("src", hero.image);
 let heroDiv = document.createElement("div");
 let heroHeading = document.createElement("h1");
@@ -14,8 +16,36 @@ let heroIcon = document.createElement("img");
 heroIcon.setAttribute("src", hero.icon);
 heroButton.append(heroIcon);
 heroDiv.append(heroHeading, heroText, heroButton);
-heroSec.append(heroImage, heroDiv);
+heroSec.append(heroImage, heroDiv); */
 
+let heroArray = hero.headline.split(" ");
+console.log(heroArray);
+let part1 = heroArray.slice(0, 5).join(" ");
+let part2 = heroArray.slice(5, 8).join(" ");
+let part3 = heroArray.slice(8).join(" ");
+console.log(part1);
+console.log(part2);
+console.log(part3);
+const heroString = /*html*/`
+    <figure>
+        <img src="${hero.image}" alt="${hero.headline}">
+        <figcaption>
+            <h1>
+                <span>${part1}</span>
+                <span class="hero-part2">${part2}</span>
+                <span>${part3}</span>
+            </h1>
+            <p>${hero.copy}</p>
+            <button>
+                <figure><img src="${hero.icon}" alt="Globe icon"></figure>
+                <span>Explore</span>
+            </button>
+        </figcaption>
+    </figure>
+`;
+heroSec.insertAdjacentHTML("afterbegin", heroString);
+
+// Service
 const servicesSec = document.querySelector(".services");
 services.forEach(service => {
     let serviceDiv = document.createElement("div");
@@ -33,6 +63,7 @@ services.forEach(service => {
     servicesSec.appendChild(serviceDiv);
 });
 
+// Facility
 const facilitiesSec = document.querySelector(".facilities");
 let facilitiesHeadline = document.createElement("h2");
 facilitiesHeadline.textContent = facilities.headline;
@@ -51,6 +82,7 @@ facilities.options.forEach(option => {
     facilitiesSec.append(facilitiesDiv);
 });
 
+// Sites
 const sitesSec = document.querySelector(".sites");
 let sitesDiv = document.createElement("div");
 let sitesHeadline = document.createElement("h2");
@@ -78,6 +110,7 @@ sites.places.forEach(place => {
     sitesSec.append(siteDiv);
 });
 
+// Advantages
 const advantagesSec = document.querySelector(".advantages");
 let advantagesHeadline = document.createElement("h2");
 advantagesHeadline.textContent = "Our Advantages";
@@ -95,6 +128,7 @@ advantages.forEach(advantage => {
     advantagesSec.appendChild(advantageDiv);
 });
 
+// Footer
 const footer = document.querySelector(".footer");
 let footerSmall = document.createElement("h3");
 footerSmall.textContent = footerData.smallHeadline;
@@ -134,4 +168,4 @@ footerNavigation.append(firstItem, secondItem, thirdItem, fourthItem, fifthItem)
 footer.append(footerCopy, footerNavigation);
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
+/* services.forEach(service => console.log(service.headline)) */
